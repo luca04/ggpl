c8f651f00f052f74d85e155fdd3c51aadf9c20c0
Luca <manciniluca02@gmail.com>
2016-10-15 21:16:16 +0200

    workshop01

---
+++ 2016-10-14/workshop_01.py
@@ -0,0 +1,50 @@
+from larlib import *
+
+def buildingStruct(pillarSec, beamSec, pillarDist, beamDist):
+	"""
+    funzione buildingStruct: crea una struttura di colonne e travi il cui numero e' dato dalle lunghezze delle liste pillarDist e beamDist.
+    parametro beamSec: dimensione di una singola trave lungo asse x e z.
+    parametro pillarSec: dimensione di un singolo pilastro lungo asse x e y.
+    parametro pillarDist: lista di distanze tra i pilastri.
+    parametro beamDist: lista di distanze tra le travi.
+    La funzione ritorna un oggetto HPC
+    """
+
+	(px,py) = pillarSec
+	(bx,bz) = beamSec
+
+#CREAZIONE DEI PILASTRI
+	pillarsX = []
+	for i in range(0, len(pillarDist)):
+		pillarsX.extend([py, -pillarDist[i]])
+	pillarsX.extend([py]) 
+
+	pillarsY = []
+	for i in range(0,len(beamDist)):
+		pillarsY.extend([beamDist[i], -bz])  
+
+	pillarsBases = PROD([Q(px),QUOTE(pillarsX)])
+	pillars = PROD([pillarsBases, QUOTE(pillarsY)])
+
+	#CREAZIONE DELLE TRAVI
+
+
+
+	beamsX = []
+	for i in range(0, len(pillarDist)):
+		beamLenght = pillarDist[i] + py
+		if i==0 or i == len(pillarDist) - 1:
+			beamLenght += py/2.0 + (py/2.0 if len(pillarDist) == 1 else 0)
+		beamsX.extend([beamLenght])
+
+	beamsY = []
+	for i in range(0,len(beamDist)):
+		beamsY.extend([-beamDist[i], bz])  
+
+	beamsBases = PROD([Q(bx),QUOTE(beamsX)])
+	beams = PROD([beamsBases, QUOTE(beamsY)])
+
+	return STRUCT([pillars,beams])
+
+s = buildingStruct((0.5, 1.0), (0.5, 0.75), [1,0.5,2.0,5,1], [1, 3, 1.5, 0.5, 3])
+skeleton = COLOR(BLUE)(SKELETON(1)(s))
