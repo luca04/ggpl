e37ccb3101690e64b61d6f7c2a7893e359aab0cc
Luca <manciniluca02@gmail.com>
2016-10-23 11:26:37 +0200

    workshop02

--- 2016-10-21/workshop_02.py
+++ 2016-10-21/workshop_02.py
@@ -13,36 +13,25 @@ def ggpl_bone_structure(file_name):
 	with open(file_name, 'rb') as f:
 		reader = csv.reader(f, delimiter=';', quotechar=',')
 		for i, row in enumerate(reader):
-			#print i
 			if (i+1)%2.0==0:
-				#print type (row[0])
-				#print (row[0])
-				#print type (row[3])
 				pillarSec = row[0]
 				pillarSec = tuple(pillarSec.split(','))
 				pillarSec = map(float,pillarSec)
-				#print (pillarSec)
 				beamSec = row[1]
 				beamSec = tuple(beamSec.split(','))
 				beamSec = map(float,beamSec)
-				#print(beamSec)
 				pillarDistY = row[2]
 				pillarDistY = pillarDistY.split(',')
 				pillarDistY = map(float,pillarDistY)
-				#print (pillarDistY)
 				beamDistY = row[3]
 				beamDistY = beamDistY.split(',')
 				beamDistY = map(float,beamDistY)
-				#print(pillarDistX)
 			else:
 				for i in row:
-					#print "ok"
 					pillarDistX.append(i)
 				pillarDistX = ''.join(pillarDistX)
 				pillarDistX = pillarDistX.split(',')
 				pillarDistX = map(float,pillarDistX)
-	#pillarSec = tuple(pillarSec.split(','))
-	#beamSec = tuple(beamSec.split(','))
 	(px,py) = pillarSec
 	(bx,bz) = beamSec
 
@@ -63,9 +52,7 @@ def ggpl_bone_structure(file_name):
 
 	pillarsBases2 = PROD([QUOTE(pillarsX2), QUOTE(pillarsX)])
 	pillarsBases = PROD([Q(px),QUOTE(pillarsX)])
-	#pillarBasesY = PROD([QUOTE(pillarsY2), Q(px)])
 	pillars = PROD([pillarsBases2, QUOTE(pillarsY)])
-	#VIEW(pillars)
 
     #CREAZIONE DELLE TRAVI
 
@@ -92,14 +79,10 @@ def ggpl_bone_structure(file_name):
 	beamsBases2 = PROD([QUOTE(beamsX2),QUOTE(pillarsX)])
 	beams = PROD([beamsBases, QUOTE(beamsY)])
 	beams2 = PROD([beamsBases2, QUOTE(beamsY)])
-	#VIEW(beams)
-	#VIEW(beams2)
 	beamsTot = STRUCT([beams,beams2])
-	#VIEW(beamsTot)
 	
 	return STRUCT([pillars,beamsTot])
 
 s = ggpl_bone_structure("frame_data_441481.csv")
 VIEW(s)
-#skeleton = COLOR(BLUE)(SKELETON(1)(s))
-#VIEW(STRUCT([s, skeleton]))
+
